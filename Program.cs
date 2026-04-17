69042e24b65e5f843e511cebacd880c341560563
Harry4Py <73236085+Harry4Py@users.noreply.github.com>
2026-04-17 11:03:26 +0530

    Fix typo in environment check and clean up code
    Updated program.cs

--- Program.cs
+++ Program.cs
@@ -9,7 +9,8 @@ if(builder.Environment.IsDevelopment())
         options.UseSqlServer(builder.Configuration.GetConnectionString("MyDbConnection")));
     builder.Services.AddDistributedMemoryCache();
 }
-else
+
+if (!builder.Enironment.IsDevelopment)
 {
     builder.Services.AddDbContext<MyDatabaseContext>(options =>
         options.UseSqlServer(builder.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING")));
